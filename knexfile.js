5b1d46334cce991b8b05ca74bb3bf3f940c61823
Michael Hart <michael@mhart.dev>
2020-01-02 17:20:54 -0500

    Postgres tweaks

--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,32 @@
 module.exports = {
   /*=== development ===*/
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: "./database/wheelgood.db3"
+      filename: './database/wheelgood.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
+        conn.run('PRAGMA foreign_keys = ON', done);
       }
     },
     migrations: {
-      directory: "./database/migrations"
+      directory: './database/migrations'
     },
     useNullAsDefault: true
   },
 
   /*=== production ===*/
   production: {
-    client: "pg",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
+      directory: './database/migrations',
+      tableName: 'knex_migrations'
     }
   }
 };
